445b487555635d9b46f07c6631b1f57e4a1aedb8
ykm <yan936769651@gmail.com>
2018-04-09 23:45:09 +0800

    change

--- client/client.py
+++ client/client.py
@@ -10,15 +10,16 @@ import os
 import sys
 import struct
 
-ADDR = ('127.0.0.1',7000)
+ADDR = ('192.168.1.103',7000)
+#ADDR = ('127.0.0.1',7000)
 FILEPATH = r'E:\1.jpg'
 
 def cloud_client():
     cli_socket = socket_bind()
     recv_welcome(cli_socket)
     if os.path.isfile(FILEPATH):
-        fileinfo_size = struct.calcsize('128sl')
-        fhead = struct.pack('128sl',os.path.basename(FILEPATH).encode('utf-8'),os.stat(FILEPATH).st_size)
+        fileinfo_size = struct.calcsize('128sd')
+        fhead = struct.pack('128sd',os.path.basename(FILEPATH).encode('utf-8'),os.stat(FILEPATH).st_size)
         cli_socket.send(fhead)
 
         fp = open(FILEPATH,'rb')
