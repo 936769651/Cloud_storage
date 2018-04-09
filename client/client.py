f5d14526aff92914ae7cc9e5fdf570908d1ea9eb
ykm <yan936769651@gmail.com>
2018-04-09 22:21:20 +0800

    change

--- client/client.py
+++ client/client.py
@@ -10,17 +10,39 @@ import os
 import sys
 import struct
 
+ADDR = ('127.0.0.1',7000)
+FILEPATH = r'E:\1.jpg'
 
-def socket_client():
+def cloud_client():
+    cli_socket = socket_bind()
+    recv_welcome(cli_socket)
+    if os.path.isfile(FILEPATH):
+        fileinfo_size = struct.calcsize('128sl')
+        fhead = struct.pack('128sl',os.path.basename(FILEPATH).encode('utf-8'),os.stat(FILEPATH).st_size)
+        cli_socket.send(fhead)
+
+        fp = open(FILEPATH,'rb')
+        while True:
+            data = fp.read(1024)
+            if not data:
+                print('{0} file send over...'.format(FILEPATH))
+                break
+            cli_socket.send(data)
+        cli_socket.close()
+
+def recv_welcome(cli_socket):
+    welcome = cli_socket.recv(1024)
+    print(welcome)
+
+def socket_bind():
     try:
-        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.connect(('192.168.1.103',7000))
+        cli_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        cli_socket.connect(ADDR)
     except socket.error as msg:
         print('socket_client() error: ' + msg)
         sys.exit(1)
 
-    print(s.recv(1024))
-
+    return cli_socket
 
 if __name__ == '__main__':
-    socket_client()
+    cloud_client()
--- server/server.py
+++ server/server.py
@@ -12,27 +12,61 @@ import sys
 import os
 import struct
 
+FILEPATH = r'E:\service'
+ADDR = ('127.0.0.1',7000)
 
-def socket_service():
+def cloud_service():
+    ser_socket = socket_bind()
+    while True:
+        conn, addr = ser_socket.accept()
+        new_thread = threading.Thread(target=deal_data,args=(conn,addr))
+        new_thread.start()
+
+def socket_bind():
     try:
-        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-        s.bind(('0.0.0.0',7000))    #绑定自身所有地址
-        s.listen(1)
+        ser_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        ser_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        ser_socket.bind(ADDR)    #绑定自身所有地址
     except socket.error as msg:
-        print('socket_service eroor: ' + msg)
+        print('socket_bind error: ' + msg)
         sys.exit(1)
 
-    print('Waiting connection...')
+    ser_socket.listen(3)
+    print('Socket bind success,start listen')
+    return ser_socket
 
-    while True:
-        conn,addr = s.accept()
-        t = threading.Thread(target=deal_data,args=(conn,addr))
-        t.start()
+def send_welcome(conn):
+    welcome = 'Hi,welcome to cloud storage!'.encode('utf-8')
+    conn.send(welcome)
 
 def deal_data(conn,addr):
     print('Accept new connection from {0}'.format(addr))
-    conn.send('Hi,welcome!')
+    send_welcome(conn)
+    fileinfo_size = struct.calcsize('128sl')
+    buf = conn.recv(fileinfo_size)
+
+    if buf:
+
+        filename, filesize = struct.unpack('128sl',buf)
+        filename = str(filename.strip(b'\x00'),encoding='utf-8')    #将文件名字节去除空字节，并转化为字符串
+        new_filename = os.path.join(FILEPATH,filename)
+        print(new_filename)
+        recvd_size = 0
+        fp = open(new_filename,'wb')
+        print('Start receiving...')
+
+        while not recvd_size == filesize:
+            if filesize - recvd_size > 1024:
+                data = conn.recv(1024)
+                recvd_size += len(data)
+            else:
+                data = conn.recv(filesize - recvd_size)
+                recvd_size = filesize
+            fp.write(data)
+        fp.close()
+        print('End receive...')
+    conn.close()
+
 
 if __name__ == '__main__':
-    socket_service()
+    cloud_service()
